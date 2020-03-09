160b1e193bb67a1a9d22c4f8e3b3ae4972ff10cb
Norbs89 <n_huvos@live.co.uk>
2020-03-09 16:59:44 +0000

    some styling applied

--- src/App.js
+++ src/App.js
@@ -6,6 +6,18 @@ import DisplayEvent from "./components/displayEvent";
 class App extends React.Component {
   state = { event };
 
+  componentDidMount = () => {
+    fetch(
+      "https://app.ticketmaster.com/discovery/v2/events/?apikey=c4kXaqgAq1waXtfVKdDY873kHV6wfGEi"
+    )
+      .then(res => {
+        return res.json();
+      })
+      .then(data => {
+        this.setState({ event: data._embedded });
+      });
+  };
+
   render() {
     return (
       <div className="mainDiv">
--- src/components/displayEvent.jsx
+++ src/components/displayEvent.jsx
@@ -3,23 +3,23 @@ import React from "react";
 const DisplayEvent = props => {
   const eventDetails = props.event.map(event => (
     <div key={event.id} className="eventCard">
-      <img src={event.images[2].url} alt="" />
-      <h2>{event.name}</h2>
-      <div>
-        <p>Date: {event.dates.start.localDate}</p>
-        <p>Start time: {event.dates.start.localTime}</p>
-        <a href={event.url}>Full event details</a>
-      </div>
-      <div>
-        <p>Venue: {event._embedded.venues[0].name}</p>
-        <p>City: {event._embedded.venues[0].city.name}</p>
-        <p>
+      <img src={event.images[1].url} alt="" />
+      <div className="allText">
+        <h2>{event.name}</h2>
+        <div className="eventDetails">
+          <p>Date: {event.dates.start.localDate}</p>
+          <p>Start time: {event.dates.start.localTime}</p>
+          <a href={event.url}>Full event details</a>
+        </div>
+        <div className="venueDetails">
+          <p>Venue: {event._embedded.venues[0].name}</p>
+          <p>City: {event._embedded.venues[0].city.name}</p>
           <a href={event._embedded.venues[0].url}>Venue details</a>
-        </p>
+        </div>
       </div>
     </div>
   ));
-  return <div>{eventDetails}</div>;
+  return <div className="cardContainer">{eventDetails}</div>;
 };
 
 export default DisplayEvent;
