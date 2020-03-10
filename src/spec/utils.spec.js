a1c04e7d9761e5ba3ab631f3901b4c19d6a856cf
Harriet-Blundell <harriet.blundell27@hotmail.co.uk>
2020-03-10 15:19:14 +0000

    created utility functions to populate our chart

--- src/spec/utils.spec.js
+++ src/spec/utils.spec.js
@@ -1,24 +1,28 @@
-import populateChartData from "../utils/utils";
-import event from "../hardcode";
+import event from '../hardcode';
+import { makeRefObj, populateChartData } from '../utils/utils';
 
-describe.only("format to populate chart data", () => {
-  it("returns an empty array when passed an empty array", () => {
+describe.only('format to populate chart data', () => {
+  it('returns an empty array when passed an empty array', () => {
     const input = [];
     const actual = populateChartData(input);
     const expected = [];
     expect(actual).toEqual(expected);
   });
-  it("returns an array with the added dataObject when passed an event data", () => {
-    const input = event.events[0];
-    const actual = populateChartData(input);
-    expect(actual).toEqual([{ x: 1, y: 1, label: "Sports" }]);
-  });
-  it.only("returns an array with the added dataObject when passed in multiple event data objects", () => {
+  it('returns an array with the added dataObject when passed in multiple event data objects', () => {
     const input = event.events;
     const actual = populateChartData(input);
     expect(actual).toEqual([
-      { x: 1, y: 1, label: "Sports" },
-      { x: 2, y: 1, label: "Sports2" }
+      { x: 1, y: 1, label: 'Sports' },
+      { x: 2, y: 1, label: 'Sports2' }
     ]);
+  });
+});
+
+describe('Making a reference object from our data', () => {
+  it('takes a data array and returns a reference object', () => {
+    const input = event.events;
+    const actual = makeRefObj(input);
+    const expected = { Sports: 1, Sports2: 1 };
+    expect(actual).toEqual(expected);
   });
 });
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,19 +1,35 @@
 const populateChartData = data => {
-  const dataArray = data.map(event => {
-    const dataObject = { x: 0, y: 0, label: "" };
-    if (Object.keys(event).length !== 0) {
-      if (dataObject.label === event.classifications[0].segment.name) {
-        dataObject.y = dataObject.y + 1 || 0;
-      } else {
-        dataObject.x = dataObject.x + 1 || 0;
-        dataObject.y = dataObject.y + 1 || 0;
-        dataObject.label = event.classifications[0].segment.name;
-        return dataObject;
-      }
+  const refObj = makeRefObj(data);
+
+  const labels = Object.keys(refObj);
+
+  const dataArray = [];
+
+  labels.forEach(label => {
+    const dataObj = { x: 1, y: 0, label: '' };
+    if (dataArray.length !== 0) {
+      dataObj.x = dataObj.x + dataArray.length;
     }
+    dataObj.label = label;
+    dataObj.y = refObj[label];
+    dataArray.push(dataObj);
   });
-  console.log(dataArray);
+
   return dataArray;
 };
 
-export default populateChartData;
+const makeRefObj = data => {
+  const refObj = {};
+
+  data.forEach(event => {
+    if (refObj[event.classifications[0].segment.name]) {
+      refObj[event.classifications[0].segment.name] =
+        refObj[event.classifications[0].segment.name] + 1 || 0;
+    } else {
+      refObj[event.classifications[0].segment.name] = 1;
+    }
+  });
+  return refObj;
+};
+
+export { makeRefObj, populateChartData };
